9fa7320f81009b13a47586c00c0b6c0866dabfbd
rsrax <rs.wolviepool1499@gmail.com>
2024-08-14 23:56:21 +0530

    Added ffmpeg to the list of packages to install in the liveusb/05-install_base_system.sh script.

--- liveusb/05-install_base_system.sh
+++ liveusb/05-install_base_system.sh
@@ -6,7 +6,7 @@ log() {
 
 # Pacstrap to install base system
 log "Installing base system with pacstrap..."
-pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode git btrfs-progs grub efibootmgr grub-btrfs inotify-tools timeshift vim nano networkmanager pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber reflector openssh man sudo || {
+pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode git btrfs-progs grub efibootmgr grub-btrfs inotify-tools timeshift vim nano networkmanager pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber reflector openssh man sudo ffmpeg || {
     log "Error installing base system!"
     exit 1
 }
